9792b6937ddc1b32bfdc11610ddfdb0214842722
imandel <im334@cornell.edu>
2021-05-02 15:24:39 -0400

    filepath

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const gpmfExtract = require('gpmf-extract');
 const goproTelemetry = require(`gopro-telemetry`);
 const fs = require('fs');
 const glob = require('glob-promise')
-const filePath = '/home/farlab/nissan/data/Video_Audio/Navigator/';
+const filePath = '/home/farlab/nissan/data/p5/rear_left/';
 
 const data = []
 
@@ -24,14 +24,14 @@ function bufferAppender(path, chunkSize) {
 }
 
 async function run(){
-
 	const files = await glob(filePath +'*.MP4')//, {}, async (err, files) => {
-		for (const file of files) {
+    for (const file of files) {
 			console.log(`processing ${file}`)
 				const fileData = await gpmfExtract(bufferAppender(file, 10 * 1024 * 1024))
 				data.push(fileData)
 			}
-	const telemetry = await goproTelemetry(data)
+  await console.log(data)
+	const telemetry = await goproTelemetry(data[0])
 	await fs.promises.writeFile('output.json', JSON.stringify(telemetry))
 	console.log('saved!')
 
