2f56c415866fd36e92c83cc163b63dbcc134c7aa
imandel <im334@cornell.edu>
2021-05-02 14:59:32 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const gpmfExtract = require('gpmf-extract');
+const goproTelemetry = require(`gopro-telemetry`);
+const fs = require('fs');
+const glob = require('glob-promise')
+const filePath = '/home/farlab/nissan/data/Video_Audio/Navigator/';
+
+const data = []
+
+function bufferAppender(path, chunkSize) {
+  return function (mp4boxFile) {
+    var stream = fs.createReadStream(path, { highWaterMark: chunkSize });
+    var bytesRead = 0;
+    stream.on('end', () => {
+      mp4boxFile.flush();
+    });
+    stream.on('data', chunk => {
+      var arrayBuffer = new Uint8Array(chunk).buffer;
+      arrayBuffer.fileStart = bytesRead;
+      mp4boxFile.appendBuffer(arrayBuffer);
+      bytesRead += chunk.length;
+    });
+    stream.resume();
+  };
+}
+
+async function run(){
+
+	const files = await glob(filePath +'*.MP4')//, {}, async (err, files) => {
+		for (const file of files) {
+			console.log(`processing ${file}`)
+				const fileData = await gpmfExtract(bufferAppender(file, 10 * 1024 * 1024))
+				data.push(fileData)
+			}
+	const telemetry = await goproTelemetry(data)
+	await fs.promises.writeFile('output.json', JSON.stringify(telemetry))
+	console.log('saved!')
+
+
+}
+
+run()
+
